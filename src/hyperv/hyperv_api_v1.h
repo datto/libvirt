9b80982396a8f2475777d836fe0b1e52b66cdc35
Sri Ramanujam <sramanujam@datto.com>
2016-12-02 16:44:46 -0500

    hyperv: reorganize driver code into layers
    Reorganizes driver code for API version decoupling.

---
+++ src/hyperv/hyperv_api_v1.h
@@ -0,0 +1,65 @@
+/*
+ * hyperv_api_v1.h: core driver functions for Hyper-V API version 1 hosts
+ *
+ * Copyright (C) 2016 Datto Inc.
+ * Copyright (C) 2011-2013 Matthias Bolte <matthias.bolte@googlemail.com>
+ * Copyright (C) 2009 Michael Sievers <msievers83@googlemail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __HYPERV_API_V1_H__
+# define __HYPERV_API_V1_H__
+
+#include <config.h>
+
+#include "internal.h"
+#include "datatypes.h"
+#include "virdomainobjlist.h"
+#include "virauth.h"
+#include "viralloc.h"
+#include "viruuid.h"
+
+/* Driver functions */
+const char *hyperv1ConnectGetType(virConnectPtr conn ATTRIBUTE_UNUSED);
+char *hyperv1ConnectGetHostname(virConnectPtr conn);
+int hyperv1NodeGetInfo(virConnectPtr conn, virNodeInfoPtr info);
+int hyperv1ConnectListDomains(virConnectPtr conn, int *ids, int maxids);
+int hyperv1ConnectNumOfDomains(virConnectPtr conn);
+virDomainPtr hyperv1DomainLookupByID(virConnectPtr conn, int id);
+virDomainPtr hyperv1DomainLookupByUUID(virConnectPtr conn, const unsigned char *uuid);
+virDomainPtr hyperv1DomainLookupByName(virConnectPtr conn, const char *name);
+int hyperv1DomainSuspend(virDomainPtr domain);
+int hyperv1DomainResume(virDomainPtr domain);
+int hyperv1DomainDestroyFlags(virDomainPtr domain, unsigned int flags);
+int hyperv1DomainDestroy(virDomainPtr domain);
+char *hyperv1DomainGetOSType(virDomainPtr domain ATTRIBUTE_UNUSED);
+int hyperv1DomainGetInfo(virDomainPtr domain, virDomainInfoPtr info);
+int hyperv1DomainGetState(virDomainPtr domain, int *state, int *reason,
+        unsigned int flags);
+char *hyperv1DomainGetXMLDesc(virDomainPtr domain, unsigned int flags);
+int hyperv1ConnectListDefinedDomains(virConnectPtr conn, char **const names,
+        int maxnames);
+int hyperv1ConnectNumOfDefinedDomains(virConnectPtr conn);
+int hyperv1DomainCreateWithFlags(virDomainPtr domain, unsigned int flags);
+int hyperv1DomainCreate(virDomainPtr domain);
+int hyperv1DomainIsActive(virDomainPtr domain);
+int hyperv1DomainManagedSave(virDomainPtr domain, unsigned int flags);
+int hyperv1DomainHasManagedSaveImage(virDomainPtr domain, unsigned int flags);
+int hyperv1DomainManagedSaveRemove(virDomainPtr domain, unsigned int flags);
+int hyperv1ConnectListAllDomains(virConnectPtr conn, virDomainPtr **domains,
+        unsigned int flags);
+
+#endif /* __HYPERV_API_V1_H__ */
