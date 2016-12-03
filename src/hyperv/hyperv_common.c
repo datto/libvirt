208dca11a2322b3c08c9b3ac049a7e54ec921bc7
Sri Ramanujam <sramanujam@datto.com>
2016-12-02 20:59:37 -0500

    hyperv: added a couple of missing functions

--- src/hyperv/hyperv_common.c
+++ src/hyperv/hyperv_common.c
@@ -1,3 +1,25 @@
+/*
+ * hyperv_common.h: common driver functions between Hyper-V API versions
+ *
+ * Copyright (C) 2016 Datto Inc.
+ * Copyright (C) 2011-2013 Matthias Bolte <matthias.bolte@googlemail.com>
+ * Copyright (C) 2009 Michael Sievers <msievers83@googlemail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
 #include <config.h>
 
 #include "viralloc.h"
