fae9008e9e91beac3b96b0d5dded139b9444e7c8
Sri Ramanujam <sramanujam@datto.com>
2017-04-17 16:25:51 -0400

    hyperv: support virDomainSendKey
    This commit adds support for virDomainSendKey. It also serves as an
    example of how to use the new method invocation APIs with a single
    "simple" type parameter.

--- src/hyperv/hyperv_wmi.c
+++ src/hyperv/hyperv_wmi.c
@@ -1302,6 +1302,13 @@ hypervGetMsvmMemorySettingDataList(hypervPrivate *priv, virBufferPtr query,
                                  (hypervObject **) list);
 }
 
+int hypervGetMsvmKeyboardList(hypervPrivate *priv, virBufferPtr query,
+                              Msvm_Keyboard **list)
+{
+    return hypervGetWmiClassList(priv, Msvm_Keyboard_WmiInfo, query,
+                                 (hypervObject **) list);
+}
+
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
