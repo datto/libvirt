a0fcf4ea9a9828de38c0d8163e31c17325a4e473
Sri Ramanujam <sramanujam@datto.com>
2017-06-27 15:13:27 -0400

    hyperv: Add support for virDomainSetMemory
    Introduces support for virDomainSetMemory. This also serves an an
    example for how to use the new method invocation API with a more
    complicated method, this time including an EPR and embedded param.

--- src/hyperv/hyperv_wmi.h
+++ src/hyperv/hyperv_wmi.h
@@ -33,6 +33,9 @@
 
 # define HYPERV_WQL_QUERY_INITIALIZER { NULL, NULL }
 
+# define MSVM_VIRTUALSYSTEMMANAGEMENTSERVICE_SELECTOR \
+    "CreationClassName=Msvm_VirtualSystemManagementService"
+
 int hypervVerifyResponse(WsManClient *client, WsXmlDocH response,
                          const char *detail);
 
@@ -210,12 +213,20 @@ int hypervGetMsvmVirtualSystemSettingDataList(hypervPrivate *priv,
                                               virBufferPtr query,
                                               Msvm_VirtualSystemSettingData **list);
 
+int hypervGetMsvmVirtualSystemSettingDataFromUUID(hypervPrivate *priv,
+                                                  const char *uuid_string,
+                                                  Msvm_VirtualSystemSettingData **list);
+
 int hypervGetMsvmProcessorSettingDataList(hypervPrivate *priv,
                                           virBufferPtr query,
                                           Msvm_ProcessorSettingData **list);
 
 int hypervGetMsvmMemorySettingDataList(hypervPrivate *priv, virBufferPtr query,
                                        Msvm_MemorySettingData **list);
+
+int hypervGetMsvmMemorySettingDataFromVSSD(hypervPrivate *priv,
+                                           const char *vssd_instanceid,
+                                           Msvm_MemorySettingData **list);
 
 int hypervGetMsvmKeyboardList(hypervPrivate *priv, virBufferPtr query,
                                        Msvm_Keyboard **list);
